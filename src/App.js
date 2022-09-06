a4a2d659c6de404f07885f35614d489a730d8d71
ashura-code <83760410+ashura-code@users.noreply.github.com>
2022-09-07 00:41:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
-import React, { useState} from 'react';
+import React, { useState , useEffect} from 'react';
 function App() {
 
   //js start
 
-  const [end,setEnd] = useState([]);
+  const [activity,setActivity] = useState([]);
+  const [type,setType] = useState([]);
 
-
-
+  // https://animechan.vercel.app/api/random  hourly requests
+  //https://www.boredapi.com/api/activity
+  //https://api.rei.my.id/v3/quotes
+useEffect(() => {
 fetch("https://animechan.vercel.app/api/random").then(n=>{
    n.json().then(data=>{
      console.log(data);
-    //  setEnd(data);
+     setActivity(data.quote);
+      setType(data.anime);
 
    })
 }).catch(err=>{console.log("Error"+ err)});
+},[])
 
-
-
-
+let test = JSON.stringify(type);
 
   //js end
   return (
@@ -27,7 +30,8 @@ fetch("https://animechan.vercel.app/api/random").then(n=>{
       {/* jsx  start*/}
       <center><h1>The Anime Quote Generator</h1></center>
 
-      {/* <h2>{JSON.stringify(end)}</h2> */}
+      <h2>{JSON.stringify(activity)}</h2>
+      <h2>{test}</h2>
 
       {/* jsx end */}
     </div>
